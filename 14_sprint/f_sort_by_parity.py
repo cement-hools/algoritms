9384a905b8df71a984982ce43c4807695a8a44b3
Максим Секачёв <cement-fan@ya.ru>
2020-12-22 17:14:06 +0300

    wardrobe

--- 14_sprint/f_sort_by_parity.py
+++ 14_sprint/f_sort_by_parity.py
@@ -3,30 +3,33 @@
 # В списках, которые вам достанутся, одинаковое количество четных и нечетных элементов.
 # Нужно отсортировать его в соответствии с новым законом.
 # Исходный порядок внутри групп четных и нечетных элементов менять нельзя.
+n = 1
 
-x = [int(i) for i in '4 2 5 7'.split()] or [0]  # res 4 5 2 7
-x = '4 2 5 7'.split()
-x = list(map(int, x))
+if n:
 
-res = []
-print(x)
-len_x = len(x)
+    x = [int(i) for i in '4 2 5 7'.split()]  # res 4 5 2 7
+    x = '4 2 5 7'.split()
+    x = list(map(int, x))
 
-for i in range(len_x):
-    if not i % 2:
-        for j in range(len(x)):
-            element = x[j]
-            if not element % 2:
-                res += [element]
-                x.pop(j)
-                break
-    else:
-        for j in range(len(x)):
-            element = x[j]
-            if element % 2:
-                res += [element]
-                x.pop(j)
-                break
+    res = []
+    print(x)
+    len_x = len(x)
 
-print(res)
-print(' '.join(map(str, res)))
+    for i in range(len_x):
+        if not i % 2:
+            for j in range(len(x)):
+                element = x[j]
+                if not element % 2:
+                    res += [element]
+                    x.pop(j)
+                    break
+        else:
+            for j in range(len(x)):
+                element = x[j]
+                if element % 2:
+                    res += [element]
+                    x.pop(j)
+                    break
+
+    print(res)
+    print(' '.join(map(str, res)))
